b86b13da10b3624393590d120d9fe3ccb901eb07
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-04 16:06:52 +0200

    Add google auth

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': 
+    '408896534015-q81fk4orlne3ink554bgbplejggtb6if.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 
+    'BQHWWcn11r0VSw6mXUhtzzGK',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'   
+};
