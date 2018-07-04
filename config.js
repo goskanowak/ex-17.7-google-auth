04e8773136eec0147b2f90cb4c873b14fa9b99d7
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-04 16:19:41 +0200

    Change id and key

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   'GOOGLE_CLIENT_ID': 
-    '408896534015-q81fk4orlne3ink554bgbplejggtb6if.apps.googleusercontent.com',
+    '408896534015-kcq8bco1gu14ttv2i9sr4f20ndadb4rm.apps.googleusercontent.com',
   'GOOGLE_CLIENT_SECRET': 
-    'BQHWWcn11r0VSw6mXUhtzzGK',
-  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'   
+    'Z47cWaDjOh0q1bPWOyZ2xikU',
+  'CALLBACK_URL': 
+    'http://localhost:3000/auth/google/callback'   
 };
